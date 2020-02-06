d69975bb8b33079d8b647aef9aef88789b6fbf01
rupal-gt <rupal.kataria@groundtruth.com>
2020-02-06 13:26:04 +0530

    add workflow

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       
-     		<p>Hello World This is Rupal Kataria</p>     
+     		<p>Hello World, This is Rupal Kataria</p>     
 
         
       </header>
